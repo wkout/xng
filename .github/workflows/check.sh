a806e56f453ab1bde59a0272459d44c45c38848b
wkout <casto@mail.lv>
2023-09-10 08:47:10 -0700

    Add file via upload

--- .github/workflows/check.sh
+++ .github/workflows/check.sh
@@ -12,7 +12,6 @@ else
     git config user.name GitHub
     git config user.email noreply@github.com
     echo "${UPSTREAM}" > .github/version.txt
-    rm -rf tmp
     git add .
     git commit --author "github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>" --allow-empty -m "$(git log -1 --pretty=%s)"
     git push 
